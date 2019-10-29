263437446bd607c7bc01e8e6213f30836e2d71d5
Paulo Carmino <contato@paulocarmino.com>
2019-10-28 21:34:22 -0300

    Add Quasar

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import App from './App.vue'
 import store from './store'
 import { createProvider } from './vue-apollo'
+import './quasar'
 
 Vue.config.productionTip = false
 
---
+++ src/quasar.js
@@ -0,0 +1,47 @@
+import Vue from 'vue'
+
+import './styles/quasar.styl'
+import iconSet from 'quasar/icon-set/mdi-v3.js'
+import lang from 'quasar/lang/pt-br.js'
+import '@quasar/extras/mdi-v3/mdi-v3.css'
+import {
+  Quasar, 
+  QLayout,
+  QHeader,
+  QDrawer,
+  QPageContainer,
+  QPage,
+  QToolbar,
+  QToolbarTitle,
+  QBtn,
+  QIcon,
+  QList,
+  QItem,
+  QItemSection,
+  QItemLabel,
+} from 'quasar'
+
+Vue.use(Quasar, {
+  config: {},
+  components: {
+    QLayout,
+    QHeader,
+    QDrawer,
+    QPageContainer,
+    QPage,
+    QToolbar,
+    QToolbarTitle,
+    QBtn,
+    QIcon,
+    QList,
+    QItem,
+    QItemSection,
+    QItemLabel,
+  },
+  directives: {
+  },
+  plugins: {
+  },
+  lang: lang,
+  iconSet: iconSet
+ })
