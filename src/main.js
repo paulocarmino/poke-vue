16ee780e0382748139b0e8945b8fcaa5ffd9791d
Paulo Carmino <contato@paulocarmino.com>
2019-10-28 21:23:29 -0300

    Add Apollo

--- src/main.js
+++ src/main.js
@@ -1,10 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
 import store from './store'
+import { createProvider } from './vue-apollo'
 
 Vue.config.productionTip = false
 
 new Vue({
   store,
+  apolloProvider: createProvider(),
   render: h => h(App)
 }).$mount('#app')
