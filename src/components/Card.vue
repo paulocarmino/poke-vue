e699b374b3b728b1de0b66c2476df283d0405f25
Paulo Carmino <contato@paulocarmino.com>
2019-10-31 15:33:13 -0300

    Adjust List

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -2,10 +2,12 @@
   <q-card class="pokeCardContainer" :style="{backgroundColor: pokemon.color}">
     <q-card-section class="pokeCard">
       <div class="layer" />
-      <img :src="pokemon.img" height="100%" />
       <div class="pokeCard--infoContainer">
         <span>{{ pokemon.number }}</span>
         <p>{{ pokemon.name }}</p>
+      </div>
+      <div class="pokeCard--thumbContainer">
+        <img :src="pokemon.img" height="88px" width="88px" />
       </div>
     </q-card-section>
   </q-card>
@@ -26,31 +28,46 @@ export default {
 }
 
 .pokeCard {
-  display: flex;
-  position: relative;
-  justify-content: space-between;
-  width: 100%;
-  height: 160px;
-  max-height: 160px;
-  z-index: 1;
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  /* position: relative; */
+  /* justify-content: space-between; */
+  /* width: 100%; */
+  height: 115px;
+  max-height: 115px;
+  /* z-index: 1; */
+
+  &.q-card__section {
+    padding: 8px;
+  }
 
   &--infoContainer {
-    flex: 1;
+    /* flex: 1; */
     display: flex;
     flex-direction: column;
-    align-items: flex-end;
+    /* align-items: flex-end; */
 
     span {
-      font-size: 1rem;
+      font-size: 0.6rem;
       color: #fff;
-      margin-bottom: -5px;
+      margin-bottom: -2px;
     }
 
     p {
-      font-size: 1.4rem;
+      font-size: 0.9rem;
       color: #fff;
       font-weight: 600;
-      letter-spacing: 0.5px;
+    }
+  }
+
+  &--thumbContainer {
+    height: 88px;
+    position: relative;
+
+    img {
+      position: absolute;
+      right: -4px;
+      bottom: -13px;
     }
   }
 }
@@ -58,12 +75,12 @@ export default {
 .layer {
   z-index: -1;
   position: absolute;
-  width: 300px;
-  height: 300px;
+  width: 150px;
+  height: 150px;
   content: "";
   background-color: rgba(255, 255, 255, 0.15);
   border-radius: 50%;
-  left: -90px;
-  top: 5px;
+  right: -46px;
+  bottom: -44px;
 }
 </style>
--- src/components/PokemonsList.vue
+++ src/components/PokemonsList.vue
@@ -75,11 +75,11 @@ export default {
 
 <style lang="scss">
 .pokemonsListContainer {
-  /* display: grid;
-  grid-template-columns: 1fr 1fr 1fr 1fr;
-  grid-column-gap: 10px; */
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  grid-column-gap: 10px;
   padding: 15px 15px;
-  width: 100%;
+  /* width: 100%; */
   cursor: pointer;
   filter: saturate(1.6);
 }
